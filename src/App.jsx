1d19ae8a227b72fc5a8887f0e139a9d752e913db
ghareb <otmaneanna2020@gmail.com>
2025-04-06 15:25:13 +0100

    adding a ChatbotIcom in file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,30 @@ const App = () => {
   return (
     <div className="container">
       <div className="chatbot-popup">
+        {/* Chatbot Header */}
         <div className="chat-header">
           <div className="header-info">
-            <Chatbotlcom/>
+            <Chatbotlcom />
             <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
+        </div>
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <Chatbotlcom />
+            <p className="message-text">
+              Hey there 👋 <br /> How can I help you today?
+            </p>
+          </div>
+
+          <div className="message user-message">
+            <Chatbotlcom />
+            <p className="message-text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </p>
           </div>
         </div>
       </div>
