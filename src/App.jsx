8a1a709f2d90ee1c5077547bba471aae3bdaf378
ghareb <otmaneanna2020@gmail.com>
2025-04-12 23:14:40 +0100

    adding logic code in this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
+import { useState } from "react";
 import ChatForm from "./components/ChatForm";
-
-return (
-  <div className="container">
-    <div className="chatbot-popup">
-      {/* Chatbot Header */}
-      <div className="chat-header">
-        <div className="header-info">
-          <ChatbotIcon />
-          <h2 className="logo-text">Chatbot</h2>
+import ChatbotIcon from "./components/Chatbotlcom";
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* Chatbot Header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
         </div>
-        <button className="material-symbols-rounded">
-          keyboard_arrow_down
-        </button>
-      </div>
 
-      {/* Chatbot Body */}
-      <div className="chat-body">
-        {/* Initial bot message */}
-        <div className="message bot-message">
-          <ChatbotIcon />
-          <p className="message-text">
-            Hey there 👋 <br /> How can I help you today?
-          </p>
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          {/* Initial bot message */}
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there 👋 <br /> How can I help you today?
+            </p>
+          </div>
         </div>
-      </div>
 
-      {/* Chatbot Footer */}
-      <div className="chat-footer">
-       <ChatForm/>
+        {/* Chatbot Footer */}
+        <div className="chat-footer">
+          <ChatForm />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
