773b4b19bfea82e1999ebf7c5f653f0580520327
ghareb <otmaneanna2020@gmail.com>
2025-04-12 23:02:45 +0100

    Modified in file ChatForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,36 @@
-import { useState } from "react";
-import ChatbotIcon from "./components/ChatbotIcon";
 import ChatForm from "./components/ChatForm";
 
-const App = () => {
-  const [chatHistory, setChatHistory] = useState([]);
-  return (
-    <div className="container">
-      <div className="chatbot-popup">
-        {/* Chatbot Header */}
-        <div className="chat-header">
-          <div className="header-info">
-            <ChatbotIcon />
-            <h2 className="logo-text">Chatbot</h2>
-          </div>
-          <button className="material-symbols-rounded">
-            keyboard_arrow_down
-          </button>
+return (
+  <div className="container">
+    <div className="chatbot-popup">
+      {/* Chatbot Header */}
+      <div className="chat-header">
+        <div className="header-info">
+          <ChatbotIcon />
+          <h2 className="logo-text">Chatbot</h2>
         </div>
-        {/* Chatbot Body */}
-        <div className="chat-body">
-          <div className="message bot-message">
-            <ChatbotIcon />
-            <p className="message-text">
-              Hey there 👋 <br /> How can I help you today?
-            </p>
-          </div>
+        <button className="material-symbols-rounded">
+          keyboard_arrow_down
+        </button>
+      </div>
 
-          {/* {Render the chat history dynamically} */}
-         {chatHistory.map((chat, index) => (
-          <ChatMessage key={index} chat={chat}/>
-         ))}
-         
-        </div>
-
-        {/* Chatbot Footer */}
-
-        <div className="chat-footer">
-          <ChatForm setChatHistory={setChatHistory} />
+      {/* Chatbot Body */}
+      <div className="chat-body">
+        {/* Initial bot message */}
+        <div className="message bot-message">
+          <ChatbotIcon />
+          <p className="message-text">
+            Hey there 👋 <br /> How can I help you today?
+          </p>
         </div>
       </div>
+
+      {/* Chatbot Footer */}
+      <div className="chat-footer">
+       <ChatForm/>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,26 +1,10 @@
-import { useRef } from "react";
-
-const ChatForm = ({ setChatHistory }) => {
-  const inputRef = useRef();
-
+const ChatForm = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    const userMessage = inputRef.current.value.trim();
-
-    if (!userMessage) return;
-    inputRef.current.value = "";
-
-    // Update chat history with the user's message
-
-    setChatHistory((history) => [
-      ...history,
-      { role: "user", text: userMessage },
-    ]);
   };
   return (
     <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
       <input
-        ref={inputRef}
         type="text"
         placeholder="Message..."
         className="message-input"
